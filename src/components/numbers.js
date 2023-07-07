1c8449a1028ea61ab8e5092a6dea500288d57dce
HFG43 <hernanguemes@hotmail.com>
2023-07-07 02:44:21 -0300

    Review Linters

--- src/components/numbers.js
+++ src/components/numbers.js
@@ -3,33 +3,17 @@ import PropTypes from 'prop-types';
 
 function Numbers({ number, handleClickNumber }) {
   return (
-    <div>
-      <button type="button" onClick={() => { handleClickNumber(number); }}>
+    <span>
+      <button type="button" id={number} onClick={() => { handleClickNumber(number); }}>
         {number}
       </button>
-    </div>
+    </span>
   );
 }
 Numbers.propTypes = {
   number: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
   handleClickNumber: PropTypes.func.isRequired,
 };
-
-// const NumberPad = ({ handleClick }) => (
-//   <div className="calc_num_pad">
-//     <Numbers handleClick={handleClick} number="1" />
-//     <Numbers handleClick={handleClick} number="2" />
-//     <Numbers handleClick={handleClick} number="3" />
-//     <Numbers handleClick={handleClick} number="4" />
-//     <Numbers handleClick={handleClick} number="5" />
-//     <Numbers handleClick={handleClick} number="6" />
-//     <Numbers handleClick={handleClick} number="7" />
-//     <Numbers handleClick={handleClick} number="8" />
-//     <Numbers handleClick={handleClick} number="9" />
-//     <Numbers handleClick={handleClick} className="calc_num_pad_zero" number="0" />
-//     <Numbers handleClick={handleClick} className="calc_num_pad_dot" number="." />
-//   </div>
-// );
 
 const NumberPad = ({ handleClickNumber }) => (
   <div className="calc_num_pad">
@@ -42,8 +26,8 @@ const NumberPad = ({ handleClickNumber }) => (
     <Numbers handleClickNumber={handleClickNumber} number="7" />
     <Numbers handleClickNumber={handleClickNumber} number="8" />
     <Numbers handleClickNumber={handleClickNumber} number="9" />
-    <Numbers handleClickNumber={handleClickNumber} className="calc_num_pad_zero" number="0" />
-    <Numbers handleClickNumber={handleClickNumber} className="calc_num_pad_dot" number="." />
+    <Numbers handleClickNumber={handleClickNumber} number="0" />
+    <Numbers handleClickNumber={handleClickNumber} number="." />
   </div>
 );
 
