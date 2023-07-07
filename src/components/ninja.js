9e755b03f2d08b9bd3b7769ce66a4589fbd18aa4
HFG43 <hernanguemes@hotmail.com>
2023-07-07 18:21:51 -0300

    Finish API Component

---
+++ src/components/ninja.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+function Quote() {
+  const [quote, setQuote] = useState([]);
+
+  useEffect(() => {
+    const category = 'leadership';
+    const getData = async () => {
+      try {
+        const getQuotes = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+          headers: { 'X-Api-Key': 'CIyv4UQ3v0Gefw3U1x3/Uw==rXLVt9NPIrrQePGQ' },
+          contentType: 'application/json',
+        });
+        const quotes = await getQuotes.json();
+        setQuote(quotes);
+      } catch (error) {
+        setQuote([]);
+      }
+    };
+    getData();
+  }, []);
+  return (
+    <div className="ninja_quote_container">
+      <div>{quote[0]?.author}</div>
+      <div>{quote[0]?.quote}</div>
+      <div>{quote[0]?.category}</div>
+    </div>
+  );
+}
+
+export default Quote;
