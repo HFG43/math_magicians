953c8d627051c3f8307a9ff436028377ca17c9bd
HFG43 <hernanguemes@hotmail.com>
2023-07-07 21:02:05 -0300

    Add Loading and Error to component

--- src/components/ninja.js
+++ src/components/ninja.js
@@ -2,23 +2,39 @@ import { useState, useEffect } from 'react';
 
 function Quote() {
   const [quote, setQuote] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     const category = 'leadership';
     const getData = async () => {
       try {
-        const getQuotes = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-          headers: { 'X-Api-Key': 'CIyv4UQ3v0Gefw3U1x3/Uw==rXLVt9NPIrrQePGQ' },
-          contentType: 'application/json',
-        });
+        const getQuotes = await fetch(
+          `https://api.api-ninjas.com/v1/quotes?category=${category}`,
+          {
+            headers: {
+              'X-Api-Key': 'CIyv4UQ3v0Gefw3U1x3/Uw==rXLVt9NPIrrQePGQ',
+            },
+            contentType: 'application/json',
+          },
+        );
         const quotes = await getQuotes.json();
         setQuote(quotes);
+        setIsLoading(false);
       } catch (error) {
         setQuote([]);
+        setHasError(true);
+        setIsLoading(false);
       }
     };
     getData();
   }, []);
+  if (hasError) {
+    return <div>There was an error while loading</div>;
+  }
+  if (isLoading) {
+    return <div>There info is loading... Please wait!</div>;
+  }
   return (
     <div className="ninja_quote_container">
       <div>
