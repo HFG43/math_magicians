6804f2786dc5709d18a443c2016c668494241778
iturres <emanueliturres.co@gmail.com>
2023-07-12 18:02:23 -0300

    Remove commented code on 'Calculator.test.js'

--- src/components/__tests__/components-tests/Calculator.test.js
+++ src/components/__tests__/components-tests/Calculator.test.js
@@ -1,6 +1,3 @@
-// import { render } from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
-
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Calculator from '../../calculator';
